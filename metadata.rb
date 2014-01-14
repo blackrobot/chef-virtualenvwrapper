f5411db85b780b7fb4550062d11300e20b65dc6f
Ben Keith <keitwb@gmail.com>
2014-01-14 10:25:44 -0500

    increment version

--- metadata.rb
+++ metadata.rb
@@ -3,7 +3,7 @@ maintainer_email "damonjablons@gmail.com"
 license          "Apache 2.0"
 description      "Installs/Configures virtualenvwrapper"
 long_description IO.read(File.join(File.dirname(__FILE__), 'README.md'))
-version          "0.1.0"
+version          "0.1.1"
 
 supports "ubuntu"
 depends "python"
